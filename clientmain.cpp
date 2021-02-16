fe760b4cd612727e697b09df13e20276ee811ac5
Kevin <kevinhamberg.jobb@gmail.com>
2021-02-16 18:46:49 +0100

    Är fan inte bra på att komma ihåg commits, har förmågan att skicka och svara på allt som servern och uppgiften vill ha, bara städande av kod återtår

--- clientmain.cpp
+++ clientmain.cpp
@@ -8,6 +8,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <string>
 /* You will to add includes here */
 
 // Enable if you want debugging to be printed, see examble below.
@@ -28,19 +29,35 @@ int main(int argc, char *argv[])
   char delim[]=":";
   char *Desthost=strtok(argv[1],delim);
   char *Destport=strtok(NULL,delim);
-  // *Desthost now points to a sting holding whatever came before the delimiter, ':'.
-  // *Dstport points to whatever string came after the delimiter. 
+
+  if (Desthost==NULL)
+  {
+    Desthost = (char*)"0.0.0.0";
+  }
+  if (Destport==NULL)
+  {
+    Destport = (char*)"0";
+  }
+  
+  
   int sockfd;
 	struct addrinfo hints, *servinfo, *p;
 	int rv;
-	int numbytes;
 	char buf[MAXDATASIZE];
+  char e621[] = "OK\n";
 
 	if (argc != 2) 
   {
 	  printf("Try again, enter host and port\n");
 	  exit(1);
 	}
+  else
+  {
+    int port=atoi(Destport);
+    #ifdef DEBUG 
+    printf("Host %s, and port %d.\n",Desthost,port);
+    #endif
+  }
 
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
@@ -81,20 +98,44 @@ int main(int argc, char *argv[])
 		return 2;
 	}
 
-  initCalcLib();
-  char *ptr;
-  ptr=randomType(); // Get a random arithemtic operator. 
+  if(recv(sockfd, buf,sizeof(buf),0) == -1)
+  {
+    perror("RECV");
+    exit(1);
+  }
 
-  double f1,f2,fresult;
-  int i1,i2,iresult;
+  printf("%s\n", buf);
+  if (strcmp(buf, "TEXT TCP 1.0\n\n") == 0 || strcmp(buf, "TEXT TCP 1.1\n\n") == 0)
+  {
+    printf("Matching messages");
+  }
+  else
+  {
+    perror("Not matching\n");
+    exit(1);
+  }
 
-  char *lineBuffer=NULL;
-  size_t lenBuffer=0;
-  ssize_t recv(int sockfd, char &lineBuffer, size_t len, int flags);
-  printf("Print a command: ");
-  //=getline(&lineBuffer,&lenBuffer,stdin);
+  if (send(sockfd, e621, strlen(e621), 0) == -1)
+  {
+    perror("Message not sent");
+    exit(1);
+  }
   
-  printf("got:> %s \n",lineBuffer);
+  double f1,f2,flotRes;
+  int i1,i2,intRes;
+
+  char lineBuffer[MAXDATASIZE];
+  if(recv(sockfd, lineBuffer, sizeof(lineBuffer), 0) == -1)
+  {
+    perror("Error obtaining data");
+  }
+  else
+  {
+    printf("Data obtained correctly: %s", lineBuffer);
+  }
+  printf("Print a command: ");
+  
+  printf("%s \n",lineBuffer);
 
   char command[10];
 
@@ -103,44 +144,98 @@ int main(int argc, char *argv[])
 
   printf("Command: |%s|\n",command);
   
-  if(command[0]=='f'){
+  if(command[0]=='f')
+  {
     printf("Float\t");
     rv=sscanf(lineBuffer,"%s %lg %lg",command,&f1,&f2);
     if(strcmp(command,"fadd")==0){
-      fresult=f1+f2;
-    } else if (strcmp(command, "fsub")==0){
-      fresult=f1-f2;
-    } else if (strcmp(command, "fmul")==0){
-      fresult=f1*f2;
-    } else if (strcmp(command, "fdiv")==0){
-      fresult=f1/f2;
+      flotRes=f1+f2;
+    } 
+    else if (strcmp(command, "fsub")==0)
+    {
+      flotRes=f1-f2;
+    } 
+    else if (strcmp(command, "fmul")==0)
+    {
+      flotRes=f1*f2;
+    } 
+    else if (strcmp(command, "fdiv")==0)
+    {
+      flotRes=f1/f2;
     }
-    printf("%s %8.8g %8.8g = %8.8g\n",command,f1,f2,fresult);
-  } else {
+    printf("%s %8.8g %8.8g = %8.8g\n",command,f1,f2,flotRes);
+  } 
+  else 
+  {
     printf("Int\t");
     rv=sscanf(lineBuffer,"%s %d %d",command,&i1,&i2);
-    if(strcmp(command,"add")==0){
-      iresult=i1+i2;
-    } else if (strcmp(command, "sub")==0){
-      iresult=i1-i2;
-      printf("[%s %d %d = %d ]\n",command,i1,i2,iresult);
-    } else if (strcmp(command, "mul")==0){
-      iresult=i1*i2;
-    } else if (strcmp(command, "div")==0){
+    if(strcmp(command,"add")==0)
+    {
+      intRes=i1+i2;
+    } 
+    else if (strcmp(command, "sub")==0)
+    {
+      intRes=i1-i2;
       
-      iresult=i1/i2;
-    } else {
+    } 
+    else if (strcmp(command, "mul")==0)
+    {
+      intRes=i1*i2;
+    } 
+    else if (strcmp(command, "div")==0)
+    {
+      
+      intRes=i1/i2;
+    } 
+    else 
+    {
       printf("No match\n");
     }
 
-    printf("%s %d %d = %d \n",command,i1,i2,iresult);
+    printf("%s %d %d = %d\n",command,i1,i2,intRes);
+  }
+    if (command[0] == 'f')
+    {
+      char floatSenderValue[MAXDATASIZE];
+      sprintf(floatSenderValue,"%8.8g\n",flotRes);
+      if (send(sockfd, floatSenderValue, strlen(floatSenderValue), 0) == -1)
+      {
+        perror("Error sending\n");
+        exit(1);
+      }
+      else
+      {
+        printf("Sent: %s\n", floatSenderValue);
+      }
+    }
+
+    else
+    {
+      char intSenderValue[MAXDATASIZE];
+      sprintf(intSenderValue,"%d\n", intRes);
+      if (send(sockfd, intSenderValue, strlen(intSenderValue), 0) == -1)
+      {
+        perror("Error sending\n");
+        exit(1);
+      }
+      else
+      {
+        printf("Sent: %s\n", intSenderValue);
+      }
+    }
+
+    if (recv(sockfd,buf,sizeof(buf),0) != 0)
+    {
+      perror("Failed to recieve\n");
+    }
+    else
+    {
+      printf("%s\n", buf);
+    }
+    
+    
 
   /* Do magic */
-  int port=atoi(Destport);
-#ifdef DEBUG 
-  printf("Host %s, and port %d.\n",Desthost,port);
-#endif
 
   
-}
 }
